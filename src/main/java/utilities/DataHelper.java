d841f8c5292a178218e7297e2c2e4988ba38bbfb
PhiHoc <phihoc656621@gmail.com>
2024-03-07 14:06:33 +0700

    apply faker library

---
+++ src/main/java/utilities/DataHelper.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class DataHelper {
+    Faker faker = new Faker(Locale.ENGLISH);
+    public static DataHelper getDataHelper(){
+        return new DataHelper();
+    }
+
+    public String getFirtName(){
+        return faker.name().firstName();
+    }
+    public String getLastName(){
+        return faker.name().lastName();
+    }
+
+    public String getEmail(){
+        return faker.internet().emailAddress();
+    }
+
+    public String getCompanyName(){
+        return faker.company().name();
+    }
+
+    public String getPhoneNumber(){
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public String getCity(){
+        return faker.address().city();
+    }
+
+}
